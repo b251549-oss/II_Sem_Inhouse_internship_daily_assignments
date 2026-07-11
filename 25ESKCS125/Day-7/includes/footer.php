37c92d7a698fd4834d409747936aac128e9b58d4
b251549-oss <b251549@skit.ac.in>
2026-07-11 06:05:34 +0530

    Create footer.php

---
+++ 25ESKCS125/Day-7/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-white border-t mt-auto py-10 text-center text-gray-500 text-sm">
+    Orchestr8 • HTML + CSS + PHP + MySQL<br>
+    89%+ of original project list implemented • <?= date('Y') ?>
+</footer>
+</body>
+</html>
