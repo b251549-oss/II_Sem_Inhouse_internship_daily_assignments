97a6dedc8d1322e1473e0dbe79682aa383da91e6
b251549-oss <b251549@skit.ac.in>
2026-07-11 12:13:43 +0530

    Delete 25ESKCS125/Day-7 directory

--- 25ESKCS125/Day-7/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="bg-white border-t mt-auto py-10 text-center text-gray-500 text-sm">
-    Orchestr8 • HTML + CSS + PHP + MySQL<br>
-    89%+ of original project list implemented • <?= date('Y') ?>
-</footer>
-</body>
-</html>
